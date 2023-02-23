1a2166547f87c6b3b5f60f8efefc3964f5b119e5
juan duran <juanmanuelduranrueda@gmail.com>
2023-02-23 17:14:18 -0500

    Intent creating in button action for the random number guessing, included some range verification and a pop up toast notifying the mistake

--- app/src/main/java/com/example/taller/MainActivity.kt
+++ app/src/main/java/com/example/taller/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.taller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val numberObtained = findViewById<EditText>(R.id.number)
+        val buttonSubmit = findViewById<Button>(R.id.button2)
+        buttonSubmit.setOnClickListener{
+            val result = numberObtained.text.toString()
+            val numtarg = result.toInt()
+            if(numtarg in 0..100){
+                //send the number to the next activity
+                numberObtained.setText("")
+                val intent = Intent(this, NumberGuessing::class.java)
+                intent.putExtra("number", numtarg)
+                startActivity(intent)
+            }else{
+                numberObtained.setText("")
+                Toast.makeText(this, "Ingrese un numero menor o igual a 100 y mayor o igual a 0", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
