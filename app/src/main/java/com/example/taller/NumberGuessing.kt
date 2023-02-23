3812da085fb07e0eee49a165ba5995d8a0598c59
juan duran <juanmanuelduranrueda@gmail.com>
2023-02-23 16:38:19 -0500

    Initial commit, adding visuals, no funci¿tionalities yet

---
+++ app/src/main/java/com/example/taller/NumberGuessing.kt
@@ -0,0 +1,11 @@
+package com.example.taller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumberGuessing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_guessing)
+    }
+}
