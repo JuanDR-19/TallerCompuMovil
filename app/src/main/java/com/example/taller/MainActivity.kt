e94942dafb563f763fd5df45d088d93b36c7372b
juan duran <juanmanuelduranrueda@gmail.com>
2023-02-24 12:05:39 -0500

    Spinner and JSON reader created

--- app/src/main/java/com/example/taller/MainActivity.kt
+++ app/src/main/java/com/example/taller/MainActivity.kt
@@ -1,13 +1,55 @@
 package com.example.taller
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
+
+    data class Pais(
+        val capital: String,
+        val name: String,
+        val name_int: String,
+        val initials: String
+    )
+    private fun readJSON(): ArrayList<Pais> {
+        val inputStream = resources.openRawResource(R.raw.paises)
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
+        val json = JSONObject(jsonString)
+        val countries = json.getJSONArray("paises")
+        val countriesList = ArrayList<Pais>()
+
+        for (i in 0 until countries.length()) {
+            val country = countries.getJSONObject(i)
+            val capital = country.getString("capital")
+            val countryName = country.getString("nombre_pais")
+            val countryNameInt = country.getString("nombre_pais_int")
+            val initials = country.getString("sigla")
+            var obt = Pais(capital,countryName,countryNameInt,initials)
+            countriesList.add(obt)
+        }
+        return countriesList
+    }
+    private fun changeintent(numtarg:Int, numberObtained:EditText){
+        if(numtarg in 0..1000){
+            //send the number to the next activity
+            numberObtained.setText("")
+            val intent = Intent(this, NumberGuessing::class.java)
+            intent.putExtra("number", numtarg)
+            startActivity(intent)
+        }else{
+            numberObtained.setText("")
+            Toast.makeText(this, "Ingrese un numero menor o igual a 100 y mayor o igual a 0", Toast.LENGTH_LONG).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,17 +58,11 @@ class MainActivity : AppCompatActivity() {
         buttonSubmit.setOnClickListener{
             val result = numberObtained.text.toString()
             val numtarg = result.toInt()
-            if(numtarg in 0..1000){
-                //send the number to the next activity
-                numberObtained.setText("")
-                val intent = Intent(this, NumberGuessing::class.java)
-                intent.putExtra("number", numtarg)
-                startActivity(intent)
-            }else{
-                numberObtained.setText("")
-                Toast.makeText(this, "Ingrese un numero menor o igual a 100 y mayor o igual a 0", Toast.LENGTH_LONG).show()
-            }
-
+            changeintent(numtarg,numberObtained)
         }
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val countries = readJSON()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, countries )
+        spinner.adapter = adapter
     }
 }
